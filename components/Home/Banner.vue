14e5d0b60252b1636039f27c612330b1b6bd3356
NghiaHuynh268 <nghiahuynh26081999@gmail.com>
2023-01-16 20:33:48 +0700

    return default

--- components/Home/Banner.vue
+++ components/Home/Banner.vue
@@ -28,23 +28,8 @@
 
 <script>
 import { BANNER } from "../../sources/data";
-<<<<<<< Updated upstream
-<<<<<<< HEAD
 import arrowLeft from "@images/Common/arrow-left.png";
 import arrowRight from "@images/Common/arrow-right.png";
-=======
-import arrowLeft from "@imagesCommon/arrow-left.png";
-import arrowRight from "@imagesCommon/arrow-right.png";
->>>>>>> 54a1a23a3e8e8399c806dbe39bec92c3907a3bd7
-=======
-<<<<<<< Updated upstream
-import arrowLeft from "@imagesCommon/arrow-left.png";
-import arrowRight from "@imagesCommon/arrow-right.png";
-=======
-import arrowLeft from "@images/Common/arrow-left.png";
-import arrowRight from "@images/Common/arrow-right.png";
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 export default {
   data() {
     return {
