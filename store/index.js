5afc35e8135a611b0abb5879a654c80cb07abbf8
Binhtit <devbinh@gmail.com>
2023-02-06 19:40:04 +0700

    fix bugs

---
+++ store/index.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+    list: [],
+    name: '',
+  })
+  
+  export const mutations = {
+    add(state, text) {
+      state.list.push({
+        text
+      })
+    },
+    remove(state, { todo }) {
+      state.list.splice(state.list.indexOf(todo), 1)
+    },
+    toggle(state, todo) {
+      todo.done = !todo.done
+    }
+  }
